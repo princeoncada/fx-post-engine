0c8c3869d07b943b24113ce27ce28f2d112816e0
Prince Oncada <prince.oncada@gmail.com>
2026-05-11 23:35:44 +0800

    Add CURRENCIES constant for supported currency codes

---
+++ lib/fx/currencies.ts
@@ -0,0 +1,18 @@
+// src/lib/fx/currencies.ts
+
+export const CURRENCIES = [
+    "USD",
+    "EUR",
+    "JPY",
+    "GBP",
+    "AUD",
+    "CAD",
+    "SGD",
+    "HKD",
+    "CNY",
+    "KRW",
+    "AED",
+    "SAR",
+    "MYR",
+    "THB",
+] as const;
